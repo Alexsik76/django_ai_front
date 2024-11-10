8ebc916ea3dd611bfd23643655c4fd2c31356eb4
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2024-11-10 15:20:42 +0200

    cahnged api url, removed dist

--- dist/assets/index-CM6mzuWE.js
+++
@@ -1,1 +0,0 @@
-import{c as e,o as n,w as o,a,t as p,u as r}from"./index-OSqwQIPn.js";import{V as i}from"./VMain-C-HGlw_A.js";const l={class:"pa-4"},f={__name:"index",setup(u){const s="https://ai.zpi-zp31.vn.ua/api/";return(c,t)=>(n(),e(i,null,{default:o(()=>[a("div",l,[t[0]||(t[0]=a("p",null,"Main",-1)),a("p",null,"API URL: "+p(r(s)),1)])]),_:1}))}};export{f as default};
